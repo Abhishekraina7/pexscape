f9ab934399bf7c0ffe02736a514c846c8177b468
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2024-05-16 16:25:41 +0530

    Added the functionality to open the touched image using Gesture detector into a new screen on user's device.

--- lib/Wallpaper.dart
+++ lib/Wallpaper.dart
@@ -1,8 +1,7 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:pexscape/full_image_screen.dart';
 
 
 // API KEY: KxhCp0iVqaWOYPRux5rQ0jNZpXGi8DShHRTpZpSOpGJTzA0eI0sy7rhi
@@ -34,7 +33,7 @@ class _WallpapersState extends State<Wallpapers> {
     {
       Map result = jsonDecode(value.body); // json function which will decode the body response from the api into a map of key value pairs in json format
        setState(() {
-        image = result['photos'];
+        image = result['photos']; // this functions simply updates the state and lets the builder function know that some data (photos) is being added to the image list
       });
     }
     );
@@ -50,11 +49,11 @@ class _WallpapersState extends State<Wallpapers> {
          headers: {
            'Authorization': 'KxhCp0iVqaWOYPRux5rQ0jNZpXGi8DShHRTpZpSOpGJTzA0eI0sy7rhi', // this is the api-key
          }).then((value) {
-           if(value.statusCode == 200){
-       Map result = jsonDecode(value.body);
+           if(value.statusCode == 200){ // if the response is okay
+       Map result = jsonDecode(value.body); // we store the body of the json response in a map ( key-value pairs)
        print(result);
        setState(() {
-         image.addAll(result['photos']);
+         image.addAll(result['photos']); // this function adds all data of photo key value into the image list
        });}
            else{
              print("NO response");
@@ -66,16 +65,16 @@ class _WallpapersState extends State<Wallpapers> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.black,
         leading: null,
-        title: const Center(child: Text("PexScape",style: TextStyle(fontWeight: FontWeight.w500),)),
+        title: const Center(child: Text("PexScape",style: TextStyle(fontWeight: FontWeight.w500,color: Colors.white),)),
       ),
       body: Column(
         children: [
           Expanded(
               child: Container(
-                child: GridView.builder(
-                  itemCount: image.length,
+                child: GridView.builder( // grid view to show the images from api response
+                  itemCount: image.length, // this means jitne photos api response mai ayege utni he grid items create hojayegei
                     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 2,
                          crossAxisSpacing: 5,
@@ -83,11 +82,17 @@ class _WallpapersState extends State<Wallpapers> {
                            mainAxisSpacing: 10,
                     ),
                     itemBuilder: (context, index){
-                    return ClipRRect(
-                      borderRadius: const BorderRadius.all(Radius.circular(20)),
-                      child: Container(
-                        margin: const EdgeInsets.only(left: 5.0, right: 5.0),
-                        child: Image(image: NetworkImage(image[index]['src']['tiny']),fit: BoxFit.cover),
+
+                    return GestureDetector( // GestureDetector function takes the touch as input and immediately transfers the Image url of the image to the FullImage screen
+                      onTap: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => FullImage(imageUrl: image[index]['src']['portrait'], photoGrapher_name: image[index]['photographer'],),)); // this is the path for pushing the url of the image on which the touch was detected
+                      },
+                      child: ClipRRect(
+                        borderRadius: const BorderRadius.all(Radius.circular(0)),
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 5.0, right: 5.0,top: 5.0),
+                          child: Image(image: NetworkImage(image[index]['src']['tiny']),fit: BoxFit.cover),
+                        ),
                       ),
                     );
                     }
@@ -95,18 +100,18 @@ class _WallpapersState extends State<Wallpapers> {
               ),
           ),
          const  SizedBox(height: 10),
-          InkWell(
+          InkWell( // this function takes the touch as input and output response is set by the user using the OnTap
             onTap: (){
-              LoadmoreImage();
+              LoadmoreImage(); // In our case we are calling the Loadmore function to load more images in the gridview
             },
             child: Container(
               height: 50,
               width: 200,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20.0), // Set the desired radius
-                color: Colors.blueGrey, // Set the container color
+                color: Colors.white, // Set the container color
               ),
-              child:  const Center(child: Text("Load more", style: TextStyle(color: Colors.white, fontSize: 18),)),
+              child:  const Center(child: Text("Load more", style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w700),)),
             ),
           )
         ],
