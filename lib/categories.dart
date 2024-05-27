7db65562995fa503d4a948667e6f981badb978d7
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2024-05-27 20:19:09 +0530

    Implemented the initial UI of pinterest long way to go now.

--- lib/categories.dart
+++ lib/categories.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -13,9 +16,21 @@ class Categories extends StatefulWidget {
 class _CategoriesState extends State<Categories> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.grey,
-      body:   Center(child: Text("This is category screen", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),),
+    return  Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 1,
+            height: MediaQuery.of(context).size.height * 0.4,
+            child: Container(
+              color: Colors.yellow,
+              child: const Center(child: Text("Space for creative or AI touch",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,),),),
+            ),
+          ),
+
+        ],
+      ),
     );
   }
 }
