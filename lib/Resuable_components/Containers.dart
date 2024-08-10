7b45a78645e8e241e83c556f840a7156e6f4983c
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2024-08-11 00:07:46 +0530

    made height and width as required parameters.

--- lib/Resuable_components/Containers.dart
+++ lib/Resuable_components/Containers.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
 
+
+
+
+
+
 class Containers extends StatefulWidget {
-  const Containers({super.key});
+  const Containers({super.key, required this.height, required this.width,});
+  final double height;
+  final double width;
+
 
   @override
   State<Containers> createState() => _ContainersState();
 }
+
 class _ContainersState extends State<Containers> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return  Container(
       margin: const EdgeInsets.only(left: 5.0, right: 5.0,top: 5.0),
-      height: 20,
-      width: 200,
+      height: widget.height,
+      width: widget.width,
       decoration:  BoxDecoration(
         color: Colors.grey,
-        borderRadius: BorderRadius.circular(40),
+        borderRadius: BorderRadius.circular(30),
       ),
     );
   }
